7d5d07c2b6c98c6b681cb7fa81d2cd63a6082e42
4drianC4 <camararequeadrianrafael@gmail.com>
2026-02-09 09:39:56 -0400

    vault backup: 2026-02-09 09:39:55

---
+++ Recursos/dia1.js
@@ -0,0 +1,11 @@
+function sumar(a, b) {
+    return a + b;
+}
+
+// JAVASCRIPT MODERNO (Arrow Function)
+const sumar = (a, b) => {
+    return a + b;
+}
+// RETORNO IMPLÍCITO (La forma más común en React)
+// "Si lo que sigue a la flecha es una expresión, retórnala automáticamente"
+const sumar = (a, b) => a + b;
